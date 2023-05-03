9d0cc379e5102bf25e0ae34ccfc27164489b1105
DHIRAJDHANGAR <dhadpe05@gmail.com>
2023-05-03 11:16:15 +0530

    Calculatore by redux

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-<<<<<<< HEAD
+
 import counterSlice from "./counterSlice";
 import CalculatoreSlice from "./CalculatoreSlice";
 
@@ -7,11 +7,5 @@ export const store = configureStore({
   reducer: {
     meraCounter: counterSlice,
     meraCalculator: CalculatoreSlice,
-  },
-=======
-import reducer from "./root-reducer";
-
-export default configureStore({
-  reducer,
->>>>>>> 2cb5391743a8e684349cb7b9458aaebdd5238e9c
-});
+  },}
+)
